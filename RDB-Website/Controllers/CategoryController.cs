4cbf0e09b45541098982fbfdffbf895147e9257e
muhammadhamzabutt80@gmail.com <muhammadhamzabutt80@gmail.com>
2020-01-20 01:42:51 +0500

    Crud Operations Has Been Done

---
+++ RDB-Website/Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+﻿using RDS_Entities;
+using RDS_Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RDB_Website.Controllers
+{
+    
+    public class CategoryController : Controller
+    {
+        CategoriesServices categoryservice = new CategoriesServices();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryservice.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryservice.SaveCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var category = categoryservice.GetCategory(Id);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryservice.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var category = categoryservice.GetCategory(Id);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            categoryservice.DeleteCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Details(int Id)
+        {
+            var category = categoryservice.GetCategory(Id);
+            return View(category);
+
+        }
+
+    }
+}
---
+++ RDS_Services/CategoriesServices.cs
@@ -0,0 +1,58 @@
+﻿using RDS_Database;
+using RDS_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace RDS_Services
+{
+    public class CategoriesServices
+    {
+        public Category GetCategory(int Id)
+        {
+            using (var context = new RDS_Context())
+            {
+                return context.Categories.Find(Id);
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            using (var context = new RDS_Context())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        public void SaveCategory(Category category)
+        {
+            using (var context = new RDS_Context())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+               
+                }
+            }
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new RDS_Context())
+            {
+                context.Entry(category).State = EntityState.Modified;
+                context.SaveChanges();
+
+            }
+        }
+        public void DeleteCategory(Category category)
+        {
+            using (var context = new RDS_Context())
+            {
+                context.Entry(category).State = EntityState.Deleted;
+                context.SaveChanges();
+
+            }
+        }
+        
+    }
+}
